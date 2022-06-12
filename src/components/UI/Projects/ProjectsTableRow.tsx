8a8616e136a66dfaa362f7058596182a2ceabda9
MaleBurs <maleburs@gmail.com>
2022-06-12 15:05:58 -0300

    Product modal

--- src/components/UI/Projects/ProjectsTableRow.tsx
+++ src/components/UI/Projects/ProjectsTableRow.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import { Project } from '../../types/projectTypes'
 import DeleteIcon from '@mui/icons-material/Delete';
+import CircleIcon from '@mui/icons-material/Circle';
+import { Circle } from '@mui/icons-material';
 interface  ProjectTableRowProps {
     row: Project,
     refresh: () => void
@@ -10,33 +12,39 @@ interface  ProjectTableRowProps {
 
 const  ProjectTableRow = (props:  ProjectTableRowProps) => {
     const { row, refresh } = props
-
-    const deleteItems = () => {
-        fetch(`http://localhost:2000/projects/${row._id}`, {
+    
+    const deleteItems = async () => {
+        const response = await fetch('http://localhost:2000/projects/${row._id}', {
             method: 'DELETE',
             headers: {
-                'Content-Type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+    
+            },
         })
-        .then( res => refresh() )
-        .catch(err => console.log(err))
+        return response;   
     }
 
     const [riskImpact, setRiskImpact] = useState('');
+    const [riskColor, setRiskColor] = useState('#9297A0');
 
     useEffect(() => {
         //tendria que aparecer en realidad un circulito con color representano el riesgo
         //pero primero veamos si esto funciona
         if(row.risk?.impact == 1){
             setRiskImpact('Bajo');
+            setRiskColor('#8CD867');
         }else if (row.risk?.impact == 2){
             setRiskImpact('Medio');
+            setRiskColor('#FFB20F');
         }else if (row.risk?.impact == 3){
             setRiskImpact('Alto');
-        }else {
+            setRiskColor('#E71D36');
+        }else if (row.risk?.impact == 4){
             setRiskImpact('Critico');
+            setRiskColor('#481E3A');
         }
     }, []);
+
 
     return (
         <TableRow hover key={row._id}>
@@ -46,7 +54,7 @@ const  ProjectTableRow = (props:  ProjectTableRowProps) => {
             <TableCell align="left">{row.state}</TableCell>           
             <TableCell align="left">{new Date(row.creationDate).toLocaleDateString('es-AR')}</TableCell>
             <TableCell align="left">{new Date(row.updatedDate).toLocaleDateString('es-AR')}</TableCell>
-            <TableCell align="left">{riskImpact}</TableCell>
+            <TableCell><Circle style={{alignSelf: 'left', color: riskColor,height: '4vh'}}></Circle></TableCell>
             <TableCell align="right">
                 <div className='hover:text-teal-600 text-slate-600 cursor-pointer' onClick={deleteItems}>
                     <DeleteIcon />
--- src/pages/proyectos/AddProjectModal.tsx
+++ src/pages/proyectos/AddProjectModal.tsx
@@ -10,7 +10,7 @@ interface AddProjectModalProps {
 
 const AddProjectModal = (props: AddProjectModalProps) => {
     const [type, setType] = useState('');
-    const [state, setState] = useState('');
+    const [showProductModal, setProductModal] = useState(false)
     const { onSubmit, onClose, show } = props
     const [newProject, setNewProject] = useState({
         name: "",
@@ -27,7 +27,6 @@ const AddProjectModal = (props: AddProjectModalProps) => {
     })
 
     const types = [{ value: 'desarrollo', label: 'Desarrollo', }, {value: 'soporte', label: 'Soporte'} ];
-    const states = [{ value: 'no iniciado', label: 'No Iniciado', }, {value: 'inicio', label: 'Inicio'}, {value: 'cancelado', label: 'Cancelado'}, {value: 'finalizado', label: 'Finalizado'} ];
 
     const handleChangeText = (e: any) => {
         setNewProject(({ ...newProject, [e.target.name]: e.target.value }))
@@ -45,14 +44,29 @@ const AddProjectModal = (props: AddProjectModalProps) => {
         return response
     }
 
-    const handleSubmit = async () => {
-        //va esta funcion porque asi mandamos el proyecto nuevo a la api
-        //y si esta ok ahi recien se submitea
+    const handleSubmitProductModal = async () =>{
         const response = await generateProjectUsingAPI()
         if (response.status === 200) {
-            onSubmit()
+            onSubmit();
         }
-        onSubmit();
+        setProductModal(false);
+    }
+
+    const onCloseProductoModal =async () => {
+        setProductModal(false);
+    }
+
+    const handleSubmit = async () => {
+        if(newProject.type == "desarrollo"){
+            console.log('si es de desarrollo');
+            setProductModal(true);
+        }else{
+            console.log("no es de desarrollo");
+            const response = await generateProjectUsingAPI()
+            if (response.status === 200) {
+                onSubmit();
+            }
+        }
     };
 
     const handleTypeSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -60,15 +74,30 @@ const AddProjectModal = (props: AddProjectModalProps) => {
         setNewProject(({ ...newProject, [event.target.name]: event.target.value }))
     };
 
-    const handleStateSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setState(event.target.value);
-        setNewProject(({ ...newProject, [event.target.name]: event.target.value }))
-    };
-
     return (
         <Modal onClose={onClose} open={show} >
             <div className='absolute bg-gray-200  text-slate-800 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[120vh] h-[85vh] rounded-xl shadow-lg'>
                 
+                <Modal onClose={onCloseProductoModal} open={showProductModal}>
+                    <div className='absolute bg-gray-200  text-slate-800 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70vh] h-[55vh] rounded-xl shadow-lg'>
+                        <Typography variant='h5' className={'m-10'}>Ingrese el poducto correspondiente al proyecto de desarrollo</Typography>
+                        <div className='flex mb-6 flex-row ml-[6vh]'>  
+                            <TextField required id="outlined-basic" name="productId" className='mr-8 w-80' label="Seleccione el producto asociado al proyecto" InputLabelProps={{ shrink: true}} variant="outlined" onChange={handleChangeText} /> 
+                        </div>
+                        <div className='flex mb-6 flex-row ml-[6vh]'>  </div>
+                        <div className="flex flex-row ml-[6vh]" >
+                            <div className="text-center mr-8 mb-6 w-52 border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={onCloseProductoModal} >
+                                <div className="m-4" > Cancelar</div>
+                            </div>
+                            <div className="w-10" ></div>
+                            <div className="text-center mr-8 mb-6 w-52  border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleSubmitProductModal}>
+                                <div className="m-4" > Crear Proyecto</div>
+                        </div>
+                    </div>
+
+                    </div>
+                </Modal>
+
                 <Typography variant='h5' className={'m-10'}>Ingrese los datos para el nuevo proyecto</Typography>
                 <div className='ml-10 flex flex-col items-center'>
                     <div className='flex mb-6 flex-row'>
@@ -83,26 +112,24 @@ const AddProjectModal = (props: AddProjectModalProps) => {
                                 </MenuItem>
                             ))}
                         </TextField>
-                        <div className='mr-8 w-80'/>
-                    </div>
-                    <div className='flex mb-6 flex-row'>
-                        <TextField required select value={state} id="outlined-basic" name="state" className='mr-8 w-80' label="Seleccione un estado inicial" variant="outlined" onChange={handleStateSelection}>
-                            {states.map((option) => (
-                                <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                        <TextField required id="outlined-basic" name="productId" className='mr-8 w-80' label="Seleccione el producto asociado al proyecto" InputLabelProps={{ shrink: true}} variant="outlined" onChange={handleChangeText} /> 
+                        <TextField required name="client" className='mr-8 w-80' label="Identifique el cliente por nombre o por CUIT" InputLabelProps={{ shrink: true}} variant="outlined" onChange={handleChangeText} 
+                            InputProps={{
+                                startAdornment: (
+                                <InputAdornment position="start">
+                                    <AccountCircle/>
+                                </InputAdornment>),}}
+                        />
                     </div>
                     <TextField id="outlined-basic" className='mb-6 w-[42rem] mr-8' name='description' label="Descripcion" multiline rows={2} InputLabelProps={{ shrink: true }} variant="outlined" onChange={handleChangeText} />
+                    <div className='flex mb-6 flex-row'></div>
+                    <div className='flex mb-6 flex-row'>  </div>
                     <div className="flex flex-row" >
                         <div className="text-center mr-8 mb-6 w-52 border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={onClose} >
                             <div className="m-4" > Cancelar</div>
                         </div>
                         <div className="w-56" ></div>
                         <div className="text-center mr-8 mb-6 w-52  border-2 border-slate-400  rounded-xl shadow-lg font-bold text-slate-800 hover:border-teal-600 hover:border-1 hover:bg-gray-200 hover:text-teal-600 transition-all duration-300 cursor-pointer" onClick={handleSubmit}>
-                            <div className="m-4" > Crear Proyecto</div>
+                            <div className="m-4" > Siguiente </div>
                         </div>
                     </div>
 
